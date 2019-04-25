c61f9ed8ff35cae25b8918c586d079f42e401f3f
Robert Tate <btate712@gmail.com>
2019-04-25 23:34:36 +0000

    Automatically backed up by Learn

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -11,8 +11,8 @@ class PetsController < ApplicationController
   end
 
   post '/pets' do
-    binding.pry
     @pet = Pet.create(params[:pet])
+    binding.pry
     if !params["owner_name"].empty?
       @pet.owner = Owner.create(name: params["owner_name"])
     end
